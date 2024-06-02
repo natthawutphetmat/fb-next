85c02800d2a9ef2fe1a18013442517cfe24bc2a3
natthawutphetmat <167544607+natthawutphetmat@users.noreply.github.com>
2024-06-02 23:11:35 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { getData } from './data';
 import Footer from './Home/footer';
 import Header from './Home/header';
 import Main from './Home/main';
+import { redirect } from 'next/navigation'
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -21,8 +22,28 @@ const Home = () => {
     fetchData();
   }, []);
 
+const gourl = () => {
+ window.location.href="https://lin.ee/g1H4k9G";
+}
+const nourl = () => {
+ window.location.href="/";
+}
+
+
+
   return (
-    <div>
+    <>
+        <head>
+         <title>PG Games สล็อตpg สล็อตเว็บตรง สล็อตแตกง่าย</title>
+        <meta name="description" content="ความแตกต่างที่เป็นตัวตัดสิน pg slot game และยังมีโปรโมชั่นแจกให้กับสมาชิกทุกท่านอีกด้วย" />
+       
+         <meta name="og:title"  content="PG Games สล็อตpg สล็อตเว็บตรง สล็อตแตกง่าย"/>
+         <meta name="og:description"  content="ความแตกต่างที่เป็นตัวตัดสิน pg slot game และยังมีโปรโมชั่นแจกให้กับสมาชิกทุกท่านอีกด้วย"/>
+         <meta name="image" content="https://www.chaiyoloan.com/img/logo.jpg"/>
+         <meta name="og:image" content="https://www.chaiyoloan.com/img/logo.jpg"/>
+
+
+       </head>
 
      <Header/>
 
@@ -30,23 +51,28 @@ const Home = () => {
      <header>
 
             <div className="box">
-            <div className="image-slide">
+            <div className="image-slide" >
+            <p  onClick={gourl} >
             {data.map(item => (
           <div key={item.id}>
             <img src={`/imgs/${item.img}`} width={400} height={400} className='img' alt='item'/>
             </div>
         ))}
+            </p>
             </div>
             </div>
 
             <div className="boxs">
             <div className="image-slide">
+              <p  onClick={nourl} >
             {data.map(item => (
           <div key={item.id}>
             <img src={`/img/${item.img}`} width={400} height={400} className='img' alt='item'/>
+         
             </div>
-            
+             
         ))}
+          </p>
             </div>
             </div>
 
@@ -65,7 +91,7 @@ const Home = () => {
       
       <Main/>
       <Footer /> 
-    </div>
+    </>
   );
 };
 
